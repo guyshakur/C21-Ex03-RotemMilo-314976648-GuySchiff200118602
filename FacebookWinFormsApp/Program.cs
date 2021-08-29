80698e700166caecd306337b8e4b78ca37e5b99f
guy schiff <guyshakur@gmail.com>
2021-08-29 09:29:06 +0300

    dfdsf

--- FacebookWinFormsApp/Program.cs
+++ FacebookWinFormsApp/Program.cs
@@ -11,7 +11,7 @@ namespace FacebookWinFormsApp
         {
             StartBasicFeatureFacebookApp();
         }
-
+        //fg
         private static void StartBasicFeatureFacebookApp()
         {
             Clipboard.SetText("design.patterns.c21");
